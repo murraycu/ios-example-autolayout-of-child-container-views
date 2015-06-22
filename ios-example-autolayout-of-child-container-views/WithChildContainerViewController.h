32e9e897ec7b9aa8605dc56fe60f015e7fbb538b
Murray Cumming <murrayc@murrayc.com>
2015-06-22 19:50:26 +0200

    Add a second ViewController in the TabBarController.

---
+++ ios-example-autolayout-of-child-container-views/WithChildContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  WithChildContainerViewController.h
+//  ios-example-autolayout-of-child-container-views
+//
+//  Created by Murray Cumming on 22/06/2015.
+//  Copyright (c) 2015 Murray Cumming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WithChildContainerViewController : UIViewController
+
+@end
---
+++ ios-example-autolayout-of-child-container-views/WithChildContainerViewController.m
@@ -0,0 +1,37 @@
+//
+//  WithChildContainerViewController.m
+//  ios-example-autolayout-of-child-container-views
+//
+//  Created by Murray Cumming on 22/06/2015.
+//  Copyright (c) 2015 Murray Cumming. All rights reserved.
+//
+
+#import "WithChildContainerViewController.h"
+
+@interface WithChildContainerViewController ()
+
+@end
+
+@implementation WithChildContainerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
